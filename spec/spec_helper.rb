92b91f5836605de80d56d83ffe1c2269839248db
Manjit Singh <manjitsingh23@gmail.com>
2014-03-24 13:54:14 +0000

    added datamapper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+# Remember environment variables from week 1?
+ENV["RACK_ENV"] = 'test' # because we need to know what database to work with
+
+# this needs to be after ENV["RACK_ENV"] = 'test' 
+# because the server needs to know
+# what environment it's running it: test or development. 
+# The environment determines what database to use.
+require './server' 
