04b011519ac942cddd1b42edc2419a4b32b3c2a7
Manjit Singh <manjitsingh23@gmail.com>
2014-03-25 12:15:46 +0000

    added tags to links

--- lib/link.rb
+++ lib/link.rb
@@ -5,6 +5,9 @@ class Link
   # this makes the instances of this class Datamapper resources
   include DataMapper::Resource
 
+  has n, :tags, :through => Resource
+
+
   # This block describes what resources our model will have
   property :id,     Serial # Serial means that it will be auto-incremented for every record
   property :title,  String
