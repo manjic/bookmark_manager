0d357c87cc02798d7a49f18bf718e1de65beba84
Manjit Singh <manjitsingh23@gmail.com>
2014-03-27 00:36:01 +0000

    addd signing out and rearranged sturcture of folders to include helper files

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,8 +7,9 @@ class Link
 
   has n, :tags, :through => Resource
 
-
   # This block describes what resources our model will have
+  # it's a good idea to have a unique id field to differentiate between
+  # records with the same title and url (if it ever happens)
   property :id,     Serial # Serial means that it will be auto-incremented for every record
   property :title,  String
   property :url,    String
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
+require_relative 'helpers/session'
 
+include SessionHelper
 
 feature "User signs in" do
 
   before(:each) do
-    User.create(:email => "test@test.com", 
-                :password => 'test', 
-                :password_confirmation => 'test')
+    User.create(:email => "test@test.com", :password => 'test', :password_confirmation => 'test')
   end
 
   scenario "with correct credentials" do
@@ -23,16 +23,24 @@ feature "User signs in" do
     expect(page).not_to have_content("Welcome, test@test.com")
   end
 
-  def sign_in(email, password)
-    visit '/sessions/new'
-    fill_in 'email', :with => email
-    fill_in 'password', :with => password
-    click_button 'Sign in'
+end
+
+feature 'User signs out' do
+
+  before(:each) do
+    User.create(:email => "test@test.com", :password => 'test', :password_confirmation => 'test')
+  end
+
+  scenario 'while being signed in' do
+    sign_in('test@test.com', 'test')
+    click_button "Sign out"
+    expect(page).to have_content("Good bye!")
+    expect(page).not_to have_content("Welcome, test@test.com")
   end
 
 end
 
-feature 'User sign ups' do
+feature "User signs up" do
 
   # Strictly speaking, the tests that check the UI 
   # (have_content, etc.) should be separate from the tests 
@@ -42,20 +50,15 @@ feature 'User sign ups' do
   # the business logic and the views.
   #
   # However, let's not worry about this yet 
-  # to keep the example simple.
+  # to keep the examples simple.
 
-  
   scenario "when being logged out" do    
     lambda { sign_up }.should change(User, :count).by(1)    
     expect(page).to have_content("Welcome, alice@example.com")
     expect(User.first.email).to eq("alice@example.com")        
   end
-  
-  scenario "with a password that doesn't match" do
-    lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0)    
-  end
 
-   scenario "with a password that doesn't match" do
+  scenario "with a password that doesn't match" do
     lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0) 
     expect(current_path).to eq('/users')   
     expect(page).to have_content("Sorry, your passwords don't match")
@@ -66,16 +69,5 @@ feature 'User sign ups' do
     lambda { sign_up }.should change(User, :count).by(0)
     expect(page).to have_content("This email is already taken")
   end
-
-  def sign_up(email = "alice@example.com", 
-              password = "oranges!", 
-              password_confirmation = "oranges!")
-    visit '/users/new'
-    fill_in :email, :with => email
-    fill_in :password, :with => password
-    fill_in :password_confirmation, :with => password_confirmation
-    click_button "Sign up"
-  end
-
 
 end
