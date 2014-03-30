79f6d50a5708a9e95bcff380795a4db0d3d5cea6
Manjit Singh <manjitsingh23@gmail.com>
2014-03-30 14:03:24 +0100

    view/links/new.erb created, route in lnks.rb and updated spec test

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -4,7 +4,7 @@ feature "User adds a new link" do
   
   scenario "when browsing the homepage" do
     expect(Link.count).to eq(0)
-    visit '/'
+    visit '/links/new'
     add_link("http://www.makersacademy.com/", "Makers Academy")
     expect(Link.count).to eq(1)
     link = Link.first
@@ -14,7 +14,7 @@ feature "User adds a new link" do
 
 
  scenario "with a few tags" do
-    visit "/"
+    visit "/links/new"
     add_link("http://www.makersacademy.com/", 
                 "Makers Academy", 
                 ['education', 'ruby'])    
